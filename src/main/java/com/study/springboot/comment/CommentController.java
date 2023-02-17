49edea6b464a6afef9687d0c0b80adbb123c5a4e
mynameiskuun <mynameiskuun@gmail.com>
2023-02-17 20:16:00 +0900

    댓글 업데이트
    댓글 insert 기능 구현 업데이트.
    db에 insert는 정상적으로 되지만 출력 관련해서 오류가 잡히질 않아 포워딩 방식을 다른 방법으로 해야 할 것 같습니다. 주말동안 구현 해보겠습니다.

--- src/main/java/com/study/springboot/comment/CommentController.java
+++ src/main/java/com/study/springboot/comment/CommentController.java
@@ -1,11 +1,13 @@
 package com.study.springboot.comment;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import lombok.RequiredArgsConstructor;
@@ -15,67 +17,77 @@ import lombok.RequiredArgsConstructor;
 public class CommentController {
 	private final CommentDao commentdao;
 
-@GetMapping("/view1")
-public void view1(Model model, int post_id) {
-	List<CommentDto> cList = commentdao.selectAllComment(post_id);
-	List<Integer> tList = new ArrayList<>();
 	
-	for (int i = 0 ; i<cList.size(); i++) {
-		//tList.add(cList.get(i).getCom_time());
-		int a = commentdao.calculateCommentTime(cList.get(i).getCom_id());
-		tList.add(a);
+	// 댓글 인서트 메소드
+	 @PostMapping("/insertComment") 
+	 public String insertComment(Model model, @RequestParam Map<String, Object> resultMap) {
+	 
+	 CommentDto commDto = new CommentDto();
+	 
+	 commDto.setMem_id(resultMap.get("mem_id").toString());
+	 commDto.setPost_id(Integer.parseInt((String)(resultMap.get("post_id"))));
+	 commDto.setCom_text(resultMap.get("com_text").toString());
+	 
+	 System.out.println("post_id :" + resultMap.get("post_id"));
+	 
+	 commentdao.insertComment(commDto);
+	 model.addAttribute("list", commentdao.selectAllComment(commDto.getPost_id()));
+	 return "/view :: #commBox"; 
+	 }
+	 
+	
+	// 대댓글 인서트 메소드
+	
+	 @PostMapping("/insertSubComment") 
+	 public String insertSubComment(Model model, @RequestParam Map<String, Object> resultMap) {
+	 
+	 CommentDto commDto = new CommentDto();
+	 
+	 commDto.setMem_id(resultMap.get("mem_id").toString());
+	 commDto.setPost_id(Integer.parseInt((String)(resultMap.get("post_id"))));
+	 commDto.setCom_text(resultMap.get("com_text").toString());
+	 
+	 model.addAttribute("list", commentdao.selectAllComment(commDto.getPost_id()));
+	 commentdao.insertComment(commDto); 
+	 
+	 return "/view :: #commBox"; }
+	 
+
+	@GetMapping("/selectAllComment")
+	public String selectAllComment(Model model, Integer post_id) {
+
+		List<CommentDto> cList = commentdao.selectAllComment(post_id);
+		model.addAttribute("list", cList);
+		System.out.println(cList);
+		
+		// List<CommentDto> cList = commentdao.selectAllComment(post_id);
+		// model.addAttribute("list", cList);
+		// int day = commentdao.calculateCommentTime(com_id);
+		// model.addAttribute("day", day);
+
+		return "view";
 	}
-	//System.out.println(tList);
-	
-	model.addAttribute("list", cList); 
-	model.addAttribute("tList", tList);   
-}
 
+	@GetMapping("/deleteComment")
+	public String deleteComment(int com_id) {
+		
+		System.out.println("com_id : " + com_id);
+		
+		try {
+			commentdao.deleteComment(com_id);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		return "redirect:/view";
+	}
 
-@ResponseBody
-@GetMapping("/insertComment")
-public String insertComment(Model model, CommentDto commDTO) {
-	
-	//System.out.println("commDTO : " + commDTO.toString());
-	commentdao.insertComment(commDTO);
-	
-	List<CommentDto> cList = commentdao.selectAllComment(commDTO.getPost_id());
-	model.addAttribute("list", cList); 
-	
-	return "asd";
-}
-  
-
-@GetMapping("/selectAllComment")
-public String selectAllComment(Model model, int com_id) {
-	
-	System.out.println("selectCommentTest");
-	//List<CommentDto> cList = commentdao.selectAllComment(post_id);
-	//model.addAttribute("list", cList);
-	//int day = commentdao.calculateCommentTime(com_id);
-	//model.addAttribute("day", day);
-	
-	return "/view";
-}
-
-
-
-@GetMapping("/deleteComment")
-public String deleteComment(int com_id) {
-	commentdao.deleteComment(com_id);
-	return "redirect:/selectAllComment";
-}
-
-
-
-@GetMapping("/asd")
-public String selectAllPost(Model model) {
-	
-	/*
-	 * List<PostDTO> pList = commentdao.selectAllPost(); model.addAttribute("post",
-	 * pList);
-	 */
-	return "my_home";
-}
+	@ResponseBody
+	@PostMapping("/ajaxTest") 
+	public String test1() {
+		
+		String content = "";
+		return "테스트용 메소드";
+	}
 
 }
--- src/main/java/com/study/springboot/post/PostController.java
+++ src/main/java/com/study/springboot/post/PostController.java
@@ -9,7 +9,6 @@ import java.util.Map;
 
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
