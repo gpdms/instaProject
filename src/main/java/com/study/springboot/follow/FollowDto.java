f85b88b4549d9b20a4e9c2e61b6c8c87849567c1
gpdms <118142992+gpdms@users.noreply.github.com>
2023-02-21 21:43:00 +0900

    follow
    팔로워,팔로잉 리스트 수정

--- src/main/java/com/study/springboot/follow/FollowDto.java
+++ src/main/java/com/study/springboot/follow/FollowDto.java
@@ -1,5 +1,7 @@
 package com.study.springboot.follow;
 
+import java.sql.Timestamp;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,4 +17,6 @@ public class FollowDto {
 	private int flw_id;
 	private String from_user;
 	private String to_user;
+	private Timestamp flw_date;
+	
 }
